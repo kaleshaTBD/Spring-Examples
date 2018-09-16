8f41b8412d618c0f3122abb6cc92915b66f086cc
Shaik Baji <Shaik Baji@192.168.10.206>
2018-09-16 15:22:47 +0530

    Second Change

--- ConstructorInjection/src/com/bean/HelloUser.java
+++ ConstructorInjection/src/com/bean/HelloUser.java
@@ -4,17 +4,23 @@ public class HelloUser {
 	
 	private int Id;
 	private String name;
+	private float salary;
+
+	
 
 
-	 public HelloUser(int id, String name) {
+	public HelloUser(int id, String name, float salary) {
 		super();
 		Id = id;
 		this.name = name;
+		this.salary = salary;
 	}
+
+
 
 
 	public void display()
 	 {
-		 System.out.println("Employee ID:"+ Id +" & Name is:"+name);
+		 System.out.println("Employee ID:"+ Id +" & Name is:"+name+"& Sal is:"+salary);
 	 }
 }
